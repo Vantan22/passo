6fb0945cfeeba1fa9fc2113b1ba02fe3b440b0f0
Vawntan <vawntanng@gmail.com>
2023-07-16 23:22:47 +0700

    [ADD] header menu and common scss color and logo files

--- src/App.vue
+++ src/App.vue
@@ -11,14 +11,11 @@ export default {
 };
 </script>
 <style>
-#app {
+* {
   padding: 0;
   margin: 0;
   box-sizing: border-box;
   color: #2c3e50;
   font-family: "Inter", sans-serif;
-}
-html {
-  background: #42b983;
 }
 </style>
