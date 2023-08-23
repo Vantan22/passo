59afaa38a836434c22bd17b0c1c4e1faceb77039
Charlie Nguyen <vawntanng@gmail.com>
2023-08-23 23:26:55 +0700

    [ADD] add-section-service and refactor button navigate

--- src/views/home/HomePage.vue
+++ src/views/home/HomePage.vue
@@ -2,15 +2,17 @@
   <div class="home">
     <hero-banner />
     <about-us />
+    <service-section />
   </div>
 </template>
 
 <script>
 import HeroBanner from "@/views/home/component/HeroBanner.vue";
 import AboutUs from "@/views/home/component/AboutUs.vue";
+import ServiceSection from "@/views/home/component/ServiceSection.vue";
 
 export default {
-  components: { AboutUs, HeroBanner },
+  components: { ServiceSection, AboutUs, HeroBanner },
 };
 </script>
 
